439424199d113cc36cfa8476f0040a476999285c
Kartikay Singh <98656350+Karti01kay@users.noreply.github.com>
2024-08-05 21:09:19 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:5000/api/v1";
+export const BASE_URL = "https://hospital-booking-backend-6w0z.onrender.com/api/v1";
 export const token = localStorage.getItem("token");
